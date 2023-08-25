29beb60c7dc6d5da72dbc39a44d0ad909eeeb320
MaxTensor <nmolonia@gmail.com>
2023-08-25 11:31:18 +0100

    space after <span>

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -61,7 +61,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span>GPT ARCH</span>
+        <span> GPT ARCH</span>
       </a>
     </span>
   )
